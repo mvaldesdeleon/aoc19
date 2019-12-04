4a6cc4584e0fab854be17f138aa9258ff26a3de9
Martín Valdés de León <m.valdesdeleon@gmail.com>
2019-12-04 23:43:10 +0100

    Day 4: List comprehensions

--- src/Day4.hs
+++ src/Day4.hs
@@ -2,11 +2,52 @@ module Day4
     ( day4
     ) where
 
-import           Paths_aoc19 (getDataFileName)
+from = 382345
 
-loadInput :: IO String
-loadInput = getDataFileName "inputs/day-4.txt" >>= readFile
+to = 843167
+
+passwords =
+    [ p
+    | a <- [3 .. 8]
+    , b <- [0 .. 9]
+    , c <- [0 .. 9]
+    , d <- [0 .. 9]
+    , e <- [0 .. 9]
+    , f <- [0 .. 9]
+    , let p = a * 100000 + b * 10000 + c * 1000 + d * 100 + e * 10 + f
+    , p >= from
+    , p <= to
+    , a == b || b == c || c == d || d == e || e == f
+    , a <= b
+    , b <= c
+    , c <= d
+    , d <= e
+    , e <= f
+    ]
+
+betterPasswords =
+    [ p
+    | a <- [3 .. 8]
+    , b <- [0 .. 9]
+    , c <- [0 .. 9]
+    , d <- [0 .. 9]
+    , e <- [0 .. 9]
+    , f <- [0 .. 9]
+    , let p = a * 100000 + b * 10000 + c * 1000 + d * 100 + e * 10 + f
+    , p >= from
+    , p <= to
+    , (a == b && b /= c) ||
+          (a /= b && b == c && c /= d) ||
+          (b /= c && c == d && d /= e) ||
+          (c /= d && d == e && e /= f) || (d /= e && e == f)
+    , a <= b
+    , b <= c
+    , c <= d
+    , d <= e
+    , e <= f
+    ]
 
 day4 :: IO ()
 day4 = do
-    undefined
+    print $ length passwords
+    print $ length betterPasswords
