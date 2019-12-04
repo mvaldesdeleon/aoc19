25b09f3ae7235eade977370e72af439462283275
Martín Valdés de León <m.valdesdeleon@gmail.com>
2019-12-04 22:36:39 +0100

    Day 4: Prework

---
+++ src/Day4.hs
@@ -0,0 +1,12 @@
+module Day4
+    ( day4
+    ) where
+
+import           Paths_aoc19 (getDataFileName)
+
+loadInput :: IO String
+loadInput = getDataFileName "inputs/day-4.txt" >>= readFile
+
+day4 :: IO ()
+day4 = do
+    undefined
