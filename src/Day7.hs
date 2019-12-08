e6dd54d40e5f2e5c035f6ee69761793d272fad7b
Martín Valdés de León <m.valdesdeleon@gmail.com>
2019-12-08 18:17:45 +0100

    Day 7: Very composable computers

--- src/Day7.hs
+++ src/Day7.hs
@@ -6,26 +6,33 @@ module Day7
 
 import           Control.Monad
 import           Data.Foldable       (traverse_)
-import           Data.List           (permutations)
+import           Data.List           (permutations, reverse)
+import           Data.Maybe          (catMaybes, fromJust, isJust, isNothing)
 import qualified Data.Vector.Mutable as V
 import           Paths_aoc19         (getDataFileName)
 
 loadInput :: IO String
 loadInput = getDataFileName "inputs/day-7.txt" >>= readFile
 
+data ExecutionState
+    = Ready
+    | WaitingInput
+    | Halted
+    deriving (Show, Eq)
+
 data ComputerState =
     ComputerState
-        { memory :: V.IOVector Integer
-        , ip     :: Integer
-        , input  :: [Integer]
-        , output :: [Integer]
-        , halted :: Bool
+        { memory         :: V.IOVector Integer
+        , ip             :: Integer
+        , input          :: [Integer]
+        , output         :: [Integer]
+        , executionState :: ExecutionState
         }
 
 parseInput :: String -> IO ComputerState
 parseInput string = do
     memory <- fromList (read $ "[" ++ string ++ "]")
-    return $ ComputerState memory 0 [] [] False
+    return $ ComputerState memory 0 [] [] Ready
   where
     fromList xs = do
         vector <- V.new (length xs)
@@ -75,7 +82,13 @@ bit True  = 1
 bit False = 0
 
 execute :: ComputerState -> IO ComputerState
-execute cs@ComputerState {memory, ip, input = is, output = os} = do
+execute cs@ComputerState { memory
+                         , ip
+                         , input = is
+                         , output = os
+                         , executionState = es
+                         } = do
+    when (es == Halted) $ error "Attempting to execute from a Halted state"
     Instruction {opcode, p1Mode, p2Mode, p3Mode} <-
         parseInstruction <$> V.read memory (fromInteger ip)
     case opcode of
@@ -84,21 +97,24 @@ execute cs@ComputerState {memory, ip, input = is, output = os} = do
             firstV <- applyMode cs p1Mode first
             secondV <- applyMode cs p2Mode second
             V.write memory (fromInteger third) (firstV + secondV)
-            return $ ComputerState memory (ip + 4) is os False
+            return $ ComputerState memory (ip + 4) is os Ready
         2 -> do
             (first:second:third:_) <- readParameters cs 3
             firstV <- applyMode cs p1Mode first
             secondV <- applyMode cs p2Mode second
             V.write memory (fromInteger third) (firstV * secondV)
-            return $ ComputerState memory (ip + 4) is os False
+            return $ ComputerState memory (ip + 4) is os Ready
         3 -> do
             (first:_) <- readParameters cs 1
-            V.write memory (fromInteger first) (head is)
-            return $ ComputerState memory (ip + 2) (tail is) os False
+            case is of
+                [] -> return $ ComputerState memory ip is os WaitingInput
+                (i:is) -> do
+                    V.write memory (fromInteger first) i
+                    return $ ComputerState memory (ip + 2) is os Ready
         4 -> do
             (first:_) <- readParameters cs 1
             value <- V.read memory (fromInteger first)
-            return $ ComputerState memory (ip + 2) is (value : os) False
+            return $ ComputerState memory (ip + 2) is (value : os) Ready
         5 -> do
             (first:second:_) <- readParameters cs 2
             firstV <- applyMode cs p1Mode first
@@ -111,7 +127,7 @@ execute cs@ComputerState {memory, ip, input = is, output = os} = do
                          else ip + 3)
                     is
                     os
-                    False
+                    Ready
         6 -> do
             (first:second:_) <- readParameters cs 2
             firstV <- applyMode cs p1Mode first
@@ -124,32 +140,28 @@ execute cs@ComputerState {memory, ip, input = is, output = os} = do
                          else ip + 3)
                     is
                     os
-                    False
+                    Ready
         7 -> do
             (first:second:third:_) <- readParameters cs 3
             firstV <- applyMode cs p1Mode first
             secondV <- applyMode cs p2Mode second
             V.write memory (fromInteger third) (bit (firstV < secondV))
-            return $ ComputerState memory (ip + 4) is os False
+            return $ ComputerState memory (ip + 4) is os Ready
         8 -> do
             (first:second:third:_) <- readParameters cs 3
             firstV <- applyMode cs p1Mode first
             secondV <- applyMode cs p2Mode second
             V.write memory (fromInteger third) (bit (firstV == secondV))
-            return $ ComputerState memory (ip + 4) is os False
-        99 -> return $ ComputerState memory ip is os True
-        _ -> do
-            print $ "invalid opcode: " ++ show opcode
-            print $ "ip: " ++ show ip
-            print $ "os: " ++ show os
-            undefined
+            return $ ComputerState memory (ip + 4) is os Ready
+        99 -> return $ ComputerState memory ip is os Halted
+        _ -> error $ "Invalid Opcode " ++ show opcode
 
 executeAll :: ComputerState -> IO ComputerState
 executeAll cs = do
     ncs <- execute cs
-    if halted ncs
-        then return ncs
-        else executeAll ncs
+    if executionState ncs == Ready
+        then executeAll ncs
+        else return ncs
 
 clone :: ComputerState -> IO ComputerState
 clone cs = do
@@ -158,31 +170,53 @@ clone cs = do
 
 newtype Computer =
     Computer
-        { runComputer :: [Integer] -> IO [Integer]
+        { runComputer :: [Integer] -> IO ([Integer], Maybe Computer)
         }
 
 fromComputerState :: ComputerState -> Computer
 fromComputerState cs =
     Computer $ \input -> do
-        newCs <- setInput input <$> clone cs
-        output <$> executeAll newCs
+        finalCs <- (setInput input <$> clone cs) >>= executeAll
+        let result = output finalCs
+        if executionState finalCs == Halted
+            then return (result, Nothing)
+            else return (result, Just $ fromComputerState finalCs {output = []})
   where
     setInput input cs = cs {input}
 
 instance Semigroup Computer where
-    ca <> cb = Computer $ runComputer ca >=> runComputer cb
+    ca <> cb =
+        Computer $ \input -> do
+            (resultA, newComputerA) <- runComputer ca input
+            (resultB, newComputerB) <- runComputer cb (reverse resultA)
+            when (isJust newComputerA && isNothing newComputerB) $
+                error "Computer partially halted: rhs"
+            when (isJust newComputerB && isNothing newComputerA) $
+                error "Computer partially halted: lhs"
+            return (resultB, sconcat $ catMaybes [newComputerA, newComputerB])
 
-prependInput :: Computer -> Integer -> Computer
-prependInput c i = Computer $ \input -> runComputer c (i : input)
+sconcat :: Semigroup a => [a] -> Maybe a
+sconcat [] = Nothing
+sconcat (a:as) =
+    case sconcat as of
+        Nothing -> Just a
+        Just as -> Just $ a <> as
 
-runAmps :: Computer -> [Integer] -> IO Integer
-runAmps c ps = head <$> (runComputer . sconcat . map (prependInput c) $ ps) [0]
+runAmps :: Computer -> [Integer] -> IO [Integer]
+runAmps c ps = do
+    primed <- forM ps (fmap (fromJust . snd) . runComputer c . pure)
+    run (fromJust . sconcat $ primed) [0]
   where
-    sconcat [a]    = a
-    sconcat (a:as) = a <> sconcat as
+    run c i = do
+        (result, newComputer) <- runComputer c i
+        case newComputer of
+            Nothing -> return result
+            Just nc -> run nc (reverse result)
 
 day7 :: IO ()
 day7 = do
     c <- fromComputerState <$> (loadInput >>= parseInput)
-    outputs <- forM (permutations [0, 1, 2, 3, 4]) (runAmps c)
+    outputs <- forM (permutations [0 .. 4]) (fmap head . runAmps c)
+    print $ maximum outputs
+    outputs <- forM (permutations [5 .. 9]) (fmap head . runAmps c)
     print $ maximum outputs
