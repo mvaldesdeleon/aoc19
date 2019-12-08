91705d763d54ea3cf5fe74c2d0d99b12e1740c34
Martín Valdés de León <m.valdesdeleon@gmail.com>
2019-12-08 18:45:29 +0100

    Day 8: Prework

---
+++ src/Day8.hs
@@ -0,0 +1,12 @@
+module Day8
+    ( day8
+    ) where
+
+import           Paths_aoc19 (getDataFileName)
+
+loadInput :: IO String
+loadInput = getDataFileName "inputs/day-8.txt" >>= readFile
+
+day8 :: IO ()
+day8 = do
+    undefined
